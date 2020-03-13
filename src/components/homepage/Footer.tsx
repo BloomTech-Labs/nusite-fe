d6e53f52751ea1f3125efe8aab14c9b1316c3cef
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-03-12 18:45:31 -0700

    homepage prototype, header, footer

---
+++ src/components/homepage/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+    <footer>
+    <nav>
+        <a href="https://partnerd.dev">Home</a>
+        <a href="https://partnerd.dev">Developers</a>
+        <a href="https://partnerd.dev">Login</a>
+        <a href="https://partnerd.dev">Register</a>
+    </nav>
+    <br />
+    <h4>© PartNerd 2020</h4>
+    </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/homepage/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+    <header>
+    <h1>PartNerd</h1>
+    <nav>
+        <a href="https://partnerd.dev">Home</a>
+        <a href="https://partnerd.dev">Developers</a>
+        <a href="https://partnerd.dev">Login</a>
+        <a href="https://partnerd.dev">Register</a>
+    </nav>
+    <h4>A web development marketplace</h4>
+    </header>
+    )
+};
+
+export default Header;
