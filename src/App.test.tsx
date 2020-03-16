eb6918781298ba98100d7ffa5ea46b1b3e03d3a1
Jose Medrano (Joe) <Joselmedrano@gmail.com>
2020-03-16 18:01:35 +0000

    -Modified script to produce a coverage directory.
    -Moved test files so test runner can see test files.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "../src/App";
+import App from "./App"
 
 test("renders learn react link", () => {
    const { getByText } = render(<App />);
-   const linkElement = getByText(/learn react/i);
-   expect(linkElement).toBeInTheDocument();
+   const welcome = getByText(/Hello You!!/i);
+   expect(welcome).toBeInTheDocument();
 });
