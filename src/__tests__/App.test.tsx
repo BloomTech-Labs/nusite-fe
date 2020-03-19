86846b0965754b6cc26c2711c28dbb6fca03cd0b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-03-18 19:07:53 -0700

    chore: moved test scripts into __tests__ folder

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import App from "../App";
+
+test("renders learn react link", () => {
+   const { getByText } = render(<App />);
+   const welcome = getByText(/Hello You!!/i);
+   expect(welcome).toBeInTheDocument();
+});
