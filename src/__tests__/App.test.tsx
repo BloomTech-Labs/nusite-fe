7941940712e1915eb14a8e10ac80a7d6ab3d3e76
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-06 19:01:58 -0700

    feat: Updated tests for App component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import { render, cleanup, RenderResult } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import App from "../App";
+afterEach(cleanup);
 
-test("renders learn react link", () => {
-   const { getByText } = render(<App />);
-   const welcome = getByText(/Hello You!!/i);
-   expect(welcome).toBeInTheDocument();
+test("App renders without crashing", (): void => {
+   const div: HTMLDivElement = document.createElement("div");
+   ReactDOM.render(<App />, div);
+   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Home page renders correctly", (): void => {
+   const app: RenderResult = render(<App />);
+   expect(app).toMatchSnapshot();
 });
