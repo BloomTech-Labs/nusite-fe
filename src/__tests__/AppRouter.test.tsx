4f7f1230de44357162b2a1fe556f1aaf3a5d454b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-07 20:11:58 -0700

    Added a test file for the AppRouter component
    Currently only one test works, I haven't figured out how to control the
    path for a Router through the testing interface. It may suggest that we
    move the Router component outside of AppRouter and into App.

--- src/__tests__/AppRouter.test.tsx
+++ src/__tests__/AppRouter.test.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { render, cleanup, RenderResult } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { render, cleanup, RenderResult } from "../test-utils";
+import { createMemoryHistory, MemoryHistory } from "history";
 import "@testing-library/jest-dom/extend-expect";
 
 import AppRouter from "../AppRouter";
 afterEach(cleanup);
 
-test("Renders 'Home' component when URL is '/'", (): void => {});
+test("It renders 'Home' component when URL is '/'", (): void => {
+   const appRouter: RenderResult = render(<AppRouter />);
 
-test("Renders 'Login' component when URL is '/login'", (): void => {});
+   const tagLine: HTMLElement = appRouter.getByText(
+      /Connecting web developers to people with great ideas./i
+   );
+   expect(tagLine.tagName).toMatch(/h1/i);
+   expect(tagLine.classList.contains("tagline")).toBeTruthy();
+});
 
-test("Renders 'Registration' component when URL is '/register'", (): void => {});
+test.skip("It renders 'Login' component when URL is '/login'", (): void => {
+   const history: MemoryHistory = createMemoryHistory();
+   history.push("/login");
 
-test("Renders 'Dashboard' component when URL is '/home'", (): void => {});
+   const appRouter: RenderResult = render(
+      <MemoryRouter initialEntries={["/login"]}>
+         <AppRouter />
+      </MemoryRouter>
+   );
+
+   const usrNameLabel: HTMLElement = appRouter.getByLabelText(/Username/);
+   const loginForm: HTMLElement | null = usrNameLabel.parentElement;
+
+   if (loginForm) {
+      expect(loginForm.tagName).toMatch(/form/i);
+      expect(loginForm.classList.contains("login-form")).toBeTruthy();
+   }
+});
+
+test.skip("Renders 'Registration' component when URL is '/register'", (): void => {});
+
+test.skip("Renders 'Dashboard' component when URL is '/home'", (): void => {});
