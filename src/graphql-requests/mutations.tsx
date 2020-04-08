11cfc533238ab39cb81bf3b78feb1eee69c92d71
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-04-08 18:55:26 -0500

    chore: Fixed a typo in the signup mutation.

--- src/graphql-requests/mutations.tsx
+++ src/graphql-requests/mutations.tsx
@@ -1,8 +1,8 @@
 import { gql } from 'apollo-boost'
 
 export const SIGNUP = gql`
-  mutation signup($username: String!, $password: String!, $first_name: String!, $last_name: String!, $email: String!, $company: String) {
-    signup(username: $username, password: $password, first_name: $first_name, last_name: $last_name email: $email, company: $company) {
+  mutation signup($username: String!, $password: String!, $first_name: String!, $last_name: String!, $email: String!) {
+    signup(username: $username, password: $password, first_name: $first_name, last_name: $last_name, email: $email) {
       token
       user {
         username
