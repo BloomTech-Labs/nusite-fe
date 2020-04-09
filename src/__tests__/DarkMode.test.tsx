e7b85e7eff908aef886924be127d935f7b64843b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-08 20:27:12 -0700

    chore: Updated DarkMode component unit tests

--- src/__tests__/DarkMode.test.tsx
+++ src/__tests__/DarkMode.test.tsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { render, cleanup, RenderResult } from "../test-utils";
+import {
+   render,
+   cleanup,
+   fireEvent,
+   RenderWithSpiesResult,
+} from "../test-utils";
 import "@testing-library/jest-dom/extend-expect";
 
 import DarkMode from "../views/_shared/DarkMode";
 afterEach(cleanup);
 
-test("It renders correctly", (): void => {
-   const { container, queryByText }: RenderResult = render(<DarkMode />);
+describe("DarkMode Component Tests", () => {
+   test("It renders correctly", (): void => {
+      const { container } = render(<DarkMode />);
+      expect(container.innerHTML).toMatch(/Click for Dark Mode/);
+   });
 
-   queryByText(/Click for Dark Mode/);
-   const darkMode: Element | null = container.querySelector(".darkMode");
-   expect(darkMode).toBeDefined();
+   test("It updates darkmode context onChange ", (): void => {
+      const {
+         container,
+         mockSpies: { setDarkMode },
+      }: RenderWithSpiesResult = render(<DarkMode />);
+
+      const darkMode: HTMLInputElement | null = container.querySelector(
+         ".darkMode input"
+      );
+      expect(darkMode).toBeDefined();
+      if (darkMode) {
+         expect(darkMode.checked).toBe(false);
+         expect(setDarkMode).not.toHaveBeenCalled();
+         fireEvent.click(darkMode, { target: { value: true } });
+         expect(setDarkMode).toHaveBeenCalledWith(true);
+         expect(darkMode.checked).toBe(true);
+      }
+   });
 });
