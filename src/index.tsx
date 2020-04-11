8abec269c0ffc4d83c47746040ec9045ae54215b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-11 10:36:28 -0700

    chore: Removed unecessary Router from index

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,5 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-   <Router>
-      <App />
-   </Router>,
-   document.getElementById("root")
+ReactDOM.render(<App />, document.getElementById("root")
 );
