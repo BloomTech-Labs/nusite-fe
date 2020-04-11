0ca3269339582bf234b031ae586983a2aca18c7a
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-11 10:40:03 -0700

    chore: Renamed App to AppProviders

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./AppProviders";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root")
-);
+ReactDOM.render(<AppProviders />, document.getElementById("root"));
