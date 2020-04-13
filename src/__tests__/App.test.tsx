07e426fa70efcd438de26acde2d3c06843ded361
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-12 22:00:30 -0700

    Adding test for Login component
    This test currently throws an error. I think it's because of our Apollo
    configuration. The next step will be to attempt to correct Apollo packages.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { render, cleanup, RenderWithSpiesResult } from "../test-utils";
-import { createMemoryHistory, MemoryHistory } from "history";
+import { LOGIN } from "../graphql-requests/mutations";
+import { MockedProvider } from "@apollo/react-testing";
 import "@testing-library/jest-dom/extend-expect";
 
 import App from "../App";
@@ -21,20 +22,37 @@ test("It renders 'Home' component when URL is '/'", (): void => {
    expect(tagLine.classList.contains("tagline")).toBeTruthy();
 });
 
-test.skip("It renders 'Login' component when URL is '/login'", (): void => {
+test("It renders 'Login' component when URL is '/login'", (): void => {
+   const mocks = [
+      {
+         request: {
+            query: LOGIN,
+            variables: { email: "Test01", password: "yourMum1234" },
+         },
+         result: {
+            data: {
+               login: { token: "test01-token", user: { username: "Test01" } },
+            },
+         },
+      },
+   ];
+
    const { getByLabelText }: RenderWithSpiesResult = render(
       <MemoryRouter initialEntries={["/login"]}>
-         <App />
+         <MockedProvider mocks={mocks} addTypename={false}>
+            <App />
+         </MockedProvider>
       </MemoryRouter>
    );
 
-   const usrNameLabel: HTMLElement = getByLabelText(/Username/);
-   const loginForm: HTMLElement | null = usrNameLabel.parentElement;
+   expect(true).toBe(false);
+   // const usrNameLabel: HTMLElement = getByLabelText(/Username/);
+   // const loginForm: HTMLElement | null = usrNameLabel.parentElement;
 
-   if (loginForm) {
-      expect(loginForm.tagName).toMatch(/form/i);
-      expect(loginForm.classList.contains("login-form")).toBe(true);
-   }
+   // if (loginForm) {
+   //    expect(loginForm.tagName).toMatch(/form/i);
+   //    expect(loginForm.classList.contains("login-form")).toBe(true);
+   // }
 });
 
 test.skip("Renders 'Registration' component when URL is '/register'", (): void => {});
