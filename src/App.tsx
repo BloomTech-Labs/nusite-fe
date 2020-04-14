c9bca2eb17f6408664c8578168cfc959e5c12fba
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-04-13 21:02:37 -0500

    fix: Removed <Header /> component from Registration to avoid duplication. Added <Header /> to App.tsx inside a fragment so it will render regardless of the route component displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import Logout from "./views/_shared/Logout";
 import Home from "./views/home/Home";
 import PrivateRoute from "./views/util/PrivateRoute";
 import Dashboard from "./views/dashboard/Dashboard";
+import Header from "./views/_shared/Header";
 
 const App = () => (
+   <>
+   <Header />
    <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
@@ -16,6 +19,7 @@ const App = () => (
       <Route exact path="/logout" component={Logout} />
       <PrivateRoute exact path="/home" component={Dashboard} />
    </Switch>
+   </>
 );
 
 export default App;
