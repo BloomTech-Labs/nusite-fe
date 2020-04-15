ae6f2d0c968f29a3fd0388850bdbad35c4d93452
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-04-15 14:14:29 -0700

    feat:adding query user to queries

--- src/graphql-requests/queries.tsx
+++ src/graphql-requests/queries.tsx
@@ -1,1 +1,13 @@
 import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+
+export const USER_QUERY = gql`
+   query UserQuery {
+      user(id: id) {
+         username
+         email
+         first_name
+         last_name
+      }
+   }
+`;
