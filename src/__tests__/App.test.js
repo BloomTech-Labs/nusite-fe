db484ff8984badf519ecab3da0041bea08072b5c
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-15 18:22:14 -0700

    feat: Updated App tests to include the main routes

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { render, cleanup } from "../test-utils";
-import { LOGIN } from "../graphql-requests/mutations";
+import { LOGIN, SIGNUP } from "../graphql-requests/mutations";
 import { MockedProvider } from "@apollo/react-testing";
 import "@testing-library/jest-dom/extend-expect";
 
@@ -33,23 +33,69 @@ test("It renders 'Login' component when URL is '/login'", () => {
       },
    };
 
-   const { getByLabelText } = render(
+   const { container } = render(
       <MockedProvider mocks={[mockLogin]} addTypename={false}>
          <MemoryRouter initialEntries={["/login"]}>
             <App />
          </MemoryRouter>
       </MockedProvider>
    );
+   const loginForm = container.querySelector(".login-form");
 
-   const usrNameLabel = getByLabelText(/Username/);
-   const loginForm = usrNameLabel.parentElement;
-
-   if (loginForm) {
-      expect(loginForm.tagName).toMatch(/form/i);
-      expect(loginForm.classList.contains("login-form")).toBe(true);
-   }
+   expect(loginForm).not.toBeNull();
+   expect(loginForm).toBeDefined();
+   expect(loginForm.tagName).toMatch(/form/i);
 });
 
-test.skip("Renders 'Registration' component when URL is '/register'", () => {});
+test("Renders 'Registration' component when URL is '/register'", () => {
+   const mockSignup = {
+      request: {
+         query: SIGNUP,
+         variables: {
+            username: "Da1337",
+            password: "iMda1337",
+            first_name: "John",
+            last_name: "Doe",
+            email: "da1337@email.com",
+         },
+      },
+      result: {
+         data: {
+            signup: { token: "da1337-token", user: { username: "Da1337" } },
+         },
+      },
+   };
 
-test.skip("Renders 'Dashboard' component when URL is '/home'", () => {});
+   const { container } = render(
+      <MockedProvider mocks={[mockSignup]} addTypename={false}>
+         <MemoryRouter initialEntries={["/register"]}>
+            <App />
+         </MemoryRouter>
+      </MockedProvider>
+   );
+   const registerForm = container.querySelector(".register-form");
+
+   expect(registerForm).not.toBeNull();
+   expect(registerForm).toBeDefined();
+   expect(registerForm.tagName).toMatch(/form/i);
+});
+
+test("Renders 'Dashboard' component when URL is '/home'", () => {
+   //set a test token to trick private route into rendering the component
+   localStorage.setItem("token", "test-token");
+   //render the component
+   const { getByText } = render(
+      <MemoryRouter initialEntries={["/home"]}>
+         <App />
+      </MemoryRouter>
+   );
+
+   //run the tests
+   const welcomeText = getByText(/Welcome to Your Dashboard/i);
+   expect(welcomeText).not.toBeNull();
+   expect(welcomeText).toBeDefined();
+   expect(welcomeText.tagName).toMatch(/h2/i);
+
+   //remove test token
+   localStorage.clear("token");
+});
