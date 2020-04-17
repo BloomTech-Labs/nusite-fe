5f8ab8ad1c6795a9d3b3db0dd44cc1982a87dec5
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-16 18:55:50 -0700

    feat: Added tests for Footer Component

---
+++ src/__tests__/Footer.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, cleanup, testLinkArray } from "../test-utils";
+import "@testing-library/jest-dom/extend-expect";
+
+import Footer from "../views/_shared/Footer";
+afterEach(cleanup);
+
+describe("Footer Component Tests", () => {
+   test("It renders with a list of links", () => {
+      const { getAllByRole } = render(<Footer />);
+
+      const footerLinks = getAllByRole("link");
+      const NUM_LINKS = 4;
+      const LINK_REFS = [
+         "/",
+         "/login",
+         "/register",
+         "mailto:dev-team@email.com",
+      ];
+
+      testLinkArray(footerLinks, NUM_LINKS, LINK_REFS);
+   });
+});
