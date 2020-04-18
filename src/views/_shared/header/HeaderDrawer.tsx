699119a241fed2cff5dace1dd38ba75ad57197c4
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-04-17 22:44:36 -0500

    -Deleted commented out lines.

--- src/views/_shared/header/HeaderDrawer.tsx
+++ src/views/_shared/header/HeaderDrawer.tsx
@@ -8,9 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
-//import { ThemeContext } from "../../context/contexts";
 
-//styling
 const useStyles = makeStyles(theme => ({
    toolbarMargin: {
       ...theme.mixins.toolbar,
