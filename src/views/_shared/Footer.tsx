f7e0d9d84d7df3447bd316799097be299dcaaae2
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-04-20 19:39:49 -0700

    fix: removed duplicate dashboard.jsx

--- src/views/_shared/Footer.tsx
+++ src/views/_shared/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
+//import { makeStyles } from "@material-ui/core/styles";
+//import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({}));
+//const useStyles = makeStyles(theme => ({}));
 
 const Footer = (props: any) => {
    return (
--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import DarkMode from "../_shared/DarkMode";
 import "../home/Home";
+import Logout from "../_shared/Logout";
+//import { Project } from "./ProjectForm";
+import User from "./User";
 
 const Dashboard = (props: any) => {
    const [name, setName] = useState("Anonymous");
@@ -19,6 +22,13 @@ const Dashboard = (props: any) => {
             <DarkMode />
             <br />
             <h2>Welcome to Your Dashboard {name}</h2>
+            <br />
+            <User />
+            <br />
+            <h3 className="logout" onClick={Logout}>
+               Logout
+            </h3>
+            <br />
          </main>
       </div>
    );
