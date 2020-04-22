034bce67f8f613af7f2a39a72c135fc4bd8cfd7b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-22 16:21:33 -0700

    chore: Added describe block to App component tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -9,116 +9,118 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 afterEach(cleanup);
 
-test("It renders 'Home' component when URL is '/'", () => {
-   const { getByText } = render(
-      <MemoryRouter>
-         <App />
-      </MemoryRouter>
-   );
-
-   const tagLine = getByText(/We are creating a marketplace/i);
-   expect(tagLine.tagName).toMatch(/p/i);
-});
-
-test("It renders 'Login' component when URL is '/login'", () => {
-   const mockLogin = {
-      request: {
-         query: LOGIN,
-         variables: { email: "Test01@email.com", password: "yourMum1234" },
-      },
-      result: {
-         data: {
-            login: { token: "test01-token", user: { username: "Test01" } },
-         },
-      },
-   };
-
-   const { getByTestId } = render(
-      <MockedProvider mocks={[mockLogin]} addTypename={false}>
-         <MemoryRouter initialEntries={["/login"]}>
+describe("Test App Routes", () => {
+   test("It renders 'Home' component when URL is '/'", () => {
+      const { getByText } = render(
+         <MemoryRouter>
             <App />
          </MemoryRouter>
-      </MockedProvider>
-   );
-   const loginForm = getByTestId("login-form");
-   screen.debug(loginForm);
+      );
 
-   expect(loginForm).not.toBeNull();
-   expect(loginForm).toBeDefined();
-   expect(loginForm.tagName).toMatch(/form/i);
-});
+      const tagLine = getByText(/We are creating a marketplace/i);
+      expect(tagLine.tagName).toMatch(/p/i);
+   });
 
-test("Renders 'Registration' component when URL is '/register'", () => {
-   const mockSignup = {
-      request: {
-         query: SIGNUP,
-         variables: {
-            username: "Da1337",
-            password: "iMda1337",
-            first_name: "John",
-            last_name: "Doe",
-            email: "da1337@email.com",
+   test("It renders 'Login' component when URL is '/login'", () => {
+      const mockLogin = {
+         request: {
+            query: LOGIN,
+            variables: { email: "Test01@email.com", password: "yourMum1234" },
          },
-      },
-      result: {
-         data: {
-            signup: { token: "da1337-token", user: { username: "Da1337" } },
-         },
-      },
-   };
-
-   const { getByTestId } = render(
-      <MockedProvider mocks={[mockSignup]} addTypename={false}>
-         <MemoryRouter initialEntries={["/register"]}>
-            <App />
-         </MemoryRouter>
-      </MockedProvider>
-   );
-   const registerForm = getByTestId("register-form");
-
-   expect(registerForm).not.toBeNull();
-   expect(registerForm).toBeDefined();
-   expect(registerForm.tagName).toMatch(/form/i);
-});
-
-test("Renders 'Dashboard' component when URL is '/home'", () => {
-   //set a test token to trick private route into rendering the component
-   localStorage.setItem("token", "test-token");
-   const mockGetUsers = {
-      request: {
-         query: GET_USER,
-         variables: {
-            id: 10,
-         },
-      },
-      result: {
-         data: {
-            user: {
-               id: "10",
-               username: "johnwick",
-               email: "johnwick@johnwick.com",
-               first_name: "John",
-               last_name: "Wick",
+         result: {
+            data: {
+               login: { token: "test01-token", user: { username: "Test01" } },
             },
          },
-      },
-   };
+      };
 
-   //render the component
-   const { getByText } = render(
-      <MockedProvider mocks={[mockGetUsers]} addTypename={false}>
-         <MemoryRouter initialEntries={["/home"]}>
-            <App />
-         </MemoryRouter>
-      </MockedProvider>
-   );
+      const { getByTestId } = render(
+         <MockedProvider mocks={[mockLogin]} addTypename={false}>
+            <MemoryRouter initialEntries={["/login"]}>
+               <App />
+            </MemoryRouter>
+         </MockedProvider>
+      );
+      const loginForm = getByTestId("login-form");
+      screen.debug(loginForm);
 
-   //run the tests
-   const welcomeText = getByText(/Welcome to Your Dashboard/i);
-   expect(welcomeText).not.toBeNull();
-   expect(welcomeText).toBeDefined();
-   expect(welcomeText.tagName).toMatch(/h2/i);
+      expect(loginForm).not.toBeNull();
+      expect(loginForm).toBeDefined();
+      expect(loginForm.tagName).toMatch(/form/i);
+   });
 
-   //remove test token
-   localStorage.clear();
+   test("Renders 'Registration' component when URL is '/register'", () => {
+      const mockSignup = {
+         request: {
+            query: SIGNUP,
+            variables: {
+               username: "Da1337",
+               password: "iMda1337",
+               first_name: "John",
+               last_name: "Doe",
+               email: "da1337@email.com",
+            },
+         },
+         result: {
+            data: {
+               signup: { token: "da1337-token", user: { username: "Da1337" } },
+            },
+         },
+      };
+
+      const { getByTestId } = render(
+         <MockedProvider mocks={[mockSignup]} addTypename={false}>
+            <MemoryRouter initialEntries={["/register"]}>
+               <App />
+            </MemoryRouter>
+         </MockedProvider>
+      );
+      const registerForm = getByTestId("register-form");
+
+      expect(registerForm).not.toBeNull();
+      expect(registerForm).toBeDefined();
+      expect(registerForm.tagName).toMatch(/form/i);
+   });
+
+   test("Renders 'Dashboard' component when URL is '/home'", () => {
+      //set a test token to trick private route into rendering the component
+      localStorage.setItem("token", "test-token");
+      const mockGetUsers = {
+         request: {
+            query: GET_USER,
+            variables: {
+               id: 10,
+            },
+         },
+         result: {
+            data: {
+               user: {
+                  id: "10",
+                  username: "johnwick",
+                  email: "johnwick@johnwick.com",
+                  first_name: "John",
+                  last_name: "Wick",
+               },
+            },
+         },
+      };
+
+      //render the component
+      const { getByText } = render(
+         <MockedProvider mocks={[mockGetUsers]} addTypename={false}>
+            <MemoryRouter initialEntries={["/home"]}>
+               <App />
+            </MemoryRouter>
+         </MockedProvider>
+      );
+
+      //run the tests
+      const welcomeText = getByText(/Welcome to Your Dashboard/i);
+      expect(welcomeText).not.toBeNull();
+      expect(welcomeText).toBeDefined();
+      expect(welcomeText.tagName).toMatch(/h2/i);
+
+      //remove test token
+      localStorage.clear();
+   });
 });
