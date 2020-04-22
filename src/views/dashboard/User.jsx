27a03f37ad4534d4fcd57b428d51f40ddf6f5737
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-21 19:39:28 -0700

    fix: Corrected getUser query and applied to User component
    Co-authored-by: Andrew Speer <nottolivc@gmail.com>

--- src/views/dashboard/User.jsx
+++ src/views/dashboard/User.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Query } from "react-apollo";
-import { GET_USERS } from "../../graphql-requests/queries";
+import { GET_USER } from "../../graphql-requests/queries";
 import "../home/Home.css";
 import TextField from "@material-ui/core/TextField";
 //import { makeStyles } from "@material-ui/core/styles";
@@ -16,50 +16,50 @@ import TextField from "@material-ui/core/TextField";
 
 const user_id = localStorage.getItem("user_id");
 
-const User = props => (
-   <Query query={GET_USERS}>
-      {({ loading, data }) => {
-         if (loading) return <p>Loading...</p>;
-         return (
-            <>
-               <div className="edit">
-                  {data.users.map(user => (
-                     <div key={user.id} value={user.username}>
-                        {user.id === `${user_id}` ? (
-                           <>
-                              <div className="boxedit">
-                                 <TextField
-                                    label="username:"
-                                    defaultValue={user.username}
-                                    variant="filled"
-                                 />
-                                 <TextField
-                                    label="email:"
-                                    defaultValue={user.email}
-                                    variant="filled"
-                                 />
-                                 <TextField
-                                    label="first name"
-                                    defaultValue={user.first_name}
-                                    variant="filled"
-                                 />
-                                 <TextField
-                                    label="last name"
-                                    defaultValue={user.last_name}
-                                    variant="filled"
-                                 />
-                              </div>
-                           </>
-                        ) : (
-                           console.log("user not found")
-                        )}
-                     </div>
-                  ))}
-               </div>
-            </>
-         );
-      }}
-   </Query>
-);
+const User = props => {
+   useEffect(() => {
+      console.log(GET_USER);
+   }, []);
+
+   return (
+      <Query query={GET_USER} variables={{ user_id }} displayName="GET_USER">
+         {({ error, data, loading }) => {
+            if (error) {
+               // console.error(error);
+               console.error(error.response);
+               return <p>Something went wrong!</p>;
+            }
+
+            if (loading) return <p>Loading...</p>;
+            return (
+               <>
+                  <div className="boxedit">
+                     <TextField
+                        label="username:"
+                        defaultValue={data.user.username}
+                        variant="filled"
+                     />
+                     <TextField
+                        label="email:"
+                        defaultValue={data.user.email}
+                        variant="filled"
+                     />
+                     <TextField
+                        label="first name"
+                        defaultValue={data.user.first_name}
+                        variant="filled"
+                     />
+                     <TextField
+                        label="last name"
+                        defaultValue={data.user.last_name}
+                        variant="filled"
+                     />
+                  </div>
+               </>
+            );
+         }}
+      </Query>
+   );
+};
 
 export default User;
