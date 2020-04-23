2a4b8c52e52871f109eea7c802a885ca7e1c1f18
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-04-22 19:35:45 -0500

    feat: configured props.history.push to push the user to /login after a successful call to the back end.

--- src/views/initiateReset/InitiateReset.tsx
+++ src/views/initiateReset/InitiateReset.tsx
@@ -4,24 +4,26 @@ import { Input } from "../_shared/Input";
 import { useMutation } from '@apollo/react-hooks'
 import { INITIATE_RESET } from "../../graphql-requests/mutations";
 
-
 interface ResetFormData {
     email: string;
 }
 
-export const InitiateReset: React.FC<ResetFormData> = (props: ResetFormData) => {
+export const InitiateReset: React.FC<ResetFormData> = (props: ResetFormData | any) => {
     const [reset] = useMutation(INITIATE_RESET)
-
     const onSubmit = ({ email } : ResetFormData) => {
-        reset({ variables: { email: email } })
+        reset({
+            variables: { email: email }
+        })
             .then(res => {
                 console.log(res)
+                props.history.push('/login')
             })
             .catch(err => alert(err.message))
     }
 
    return (
-      <div className="reset-form-container">
+       <div className="reset-form-container">
+           <h2>Forgot your password?</h2>
          <Form className="reset-form" data-testid="reset-form" onSubmit={onSubmit}>
             <Input name="email" type="email" placeholder="Email" />
          </Form>
