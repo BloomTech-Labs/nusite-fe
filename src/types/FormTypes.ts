b57c6c6c1e286b5ae07107675babd6d86e6bc760
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-04-23 14:12:10 -0500

    Moved FormResetData interface to FormTypes.ts from InitiateReset.tsx

--- src/types/FormTypes.ts
+++ src/types/FormTypes.ts
@@ -1,14 +1,18 @@
-import { RouterProps } from 'react-router'
+import { RouterProps } from "react-router";
 
 export interface RegistrationFormData extends RouterProps {
-    username: string;
-    password: string;
-    first_name: string;
-    last_name: string;
-    email: string;
+   username: string;
+   password: string;
+   first_name: string;
+   last_name: string;
+   email: string;
 }
 
 export interface LoginFormData extends RouterProps {
-    email: string;
-    password: string;
+   email: string;
+   password: string;
+}
+
+export interface ResetFormData extends RouterProps {
+   email: string;
 }
--- src/views/initiateReset/InitiateReset.tsx
+++ src/views/initiateReset/InitiateReset.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { Form } from "../_shared/Form";
 import { Input } from "../_shared/Input";
-import { useMutation } from '@apollo/react-hooks'
+import { useMutation } from "@apollo/react-hooks";
 import { INITIATE_RESET } from "../../graphql-requests/mutations";
+import { ResetFormData } from "../../types/FormTypes";
 
-interface ResetFormData {
-    email: string;
-}
-
-export const InitiateReset: React.FC<ResetFormData> = (props: ResetFormData | any) => {
-    const [reset] = useMutation(INITIATE_RESET)
-    const onSubmit = ({ email } : ResetFormData) => {
-        reset({
-            variables: { email: email }
-        })
-            .then(res => {
-                console.log(res)
-                props.history.push('/login')
-            })
-            .catch(err => alert(err.message))
-    }
+export const InitiateReset: React.FC<ResetFormData> = (
+   props: ResetFormData
+) => {
+   const [reset] = useMutation(INITIATE_RESET);
+   const onSubmit = ({ email }: ResetFormData) => {
+      reset({
+         variables: { email: email },
+      })
+         .then(res => {
+            console.log(res);
+            props.history.push("/login");
+         })
+         .catch(err => alert(err.message));
+   };
 
    return (
-       <div className="reset-form-container">
-           <h2>Forgot your password?</h2>
-         <Form className="reset-form" data-testid="reset-form" onSubmit={onSubmit}>
+      <div className="reset-form-container">
+         <h2>Forgot your password?</h2>
+         <Form
+            className="reset-form"
+            data-testid="reset-form"
+            onSubmit={onSubmit}
+         >
             <Input name="email" type="email" placeholder="Email" />
          </Form>
       </div>
