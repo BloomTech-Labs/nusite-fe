c4080a158a5246aa4d73c48852772d026ba8ffc1
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-04-23 18:56:15 -0700

    fix: changed user component to typescript with MUI

--- src/views/dashboard/User.tsx
+++ src/views/dashboard/User.tsx
@@ -24,11 +24,9 @@ const User: React.FC = (props: any) => {
    return (
       <Query query={GET_USER} variables={{ user_id }} displayName="GET_USER">
          {({ error, data, loading }: QueryResult) => {
-            // if (error) {
-            //    // console.error(error);
-            //    console.error(error.response);
-            //    return console.log(error);
-            // }
+            if (error) {
+               console.log(error);
+            }
 
             if (loading) return <p>Loading...</p>;
             return (
