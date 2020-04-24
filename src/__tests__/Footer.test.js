015d7a9e464c1b937fcd26916e287ed6666badae
BPitts8019 <bradleyj.pitts@gmail.com>
2020-04-23 18:04:40 -0700

    chore: Updated Footer component tests to match changes

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,22 +1,23 @@
+import "@testing-library/jest-dom/extend-expect";
+
 import React from "react";
 import { render, cleanup, testLinkArray } from "../test-utils";
-import "@testing-library/jest-dom/extend-expect";
+import { MemoryRouter } from "react-router";
 
 import Footer from "../views/_shared/Footer";
 afterEach(cleanup);
 
 describe("Footer Component Tests", () => {
    test("It renders with a list of links", () => {
-      const { getAllByRole } = render(<Footer />);
+      const { getAllByRole } = render(
+         <MemoryRouter>
+            <Footer />
+         </MemoryRouter>
+      );
 
       const footerLinks = getAllByRole("link");
       const NUM_LINKS = 4;
-      const LINK_REFS = [
-         "/",
-         "/login",
-         "/register",
-         "mailto:[placeholder-email]",
-      ];
+      const LINK_REFS = ["/", "/contact-us", "/login", "/register"];
 
       testLinkArray(footerLinks, NUM_LINKS, LINK_REFS);
    });
