ba010e7136ad2a434c9cc663d853c784da1c3989
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-04-28 01:55:51 -0700

    fix: cleaning up dashboard console

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -32,7 +32,7 @@ const Dashboard: React.FC = (props: any) => {
       });
    };
 
-   let user_id = localStorage.getItem("user_id");
+   //let user_id = localStorage.getItem("user_id");
    //console.log(user_id);
    {
       const [updateUser] = useMutation(UPDATE_USER);
