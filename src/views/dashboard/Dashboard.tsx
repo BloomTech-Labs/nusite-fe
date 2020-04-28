7eb122eb0cf8c3927e9e3b77576f9c9a253b3cb6
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-04-28 00:46:49 -0700

    fix: functionality for edit profile fields working, styling needed

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -4,10 +4,8 @@ import "../home/Home";
 import { useMutation } from "@apollo/react-hooks";
 import User from "./User";
 import Button from "@material-ui/core/Button";
-//import { Query, QueryResult } from "react-apollo";
 import { UPDATE_USER } from "../../graphql-requests/mutations";
-//import { GET_USER } from "../../graphql-requests/queries";
-let user_id = localStorage.getItem("user_id");
+import TextField from "@material-ui/core/TextField";
 
 const Dashboard: React.FC = (props: any) => {
    const [name, setName] = useState("");
@@ -17,54 +15,106 @@ const Dashboard: React.FC = (props: any) => {
          setName(username);
       }
    }, []);
-   console.log(user_id);
 
-   const [updateUser] = useMutation(UPDATE_USER);
-   const onSubmit = ({ first_name, last_name, username, email }: any) => {
-      console.log(localStorage.getItem("user_id"));
-      let user_id = localStorage.getItem("user_id");
-      updateUser({
-         variables: {
-            user_id,
-            first_name: first_name,
-            last_name: last_name,
-            username: username,
-            email: email,
-         },
-      })
-         .then(res => {
-            console.log(res.data);
-            props.history.push("/home");
-            console.log("Successfully updated.");
-         })
-         .catch(err => {
-            alert(err.message);
-            console.log(err);
-         });
+   const [users, setUsers] = useState({
+      username: "",
+      email: "",
+      first_name: "",
+      last_name: "",
+   });
+
+   const handleChange = (event: any) => {
+      // event.preventDefault();
+      setUsers({
+         ...users,
+         [event.target.name]: event.target.value,
+      });
    };
-   return (
-      <>
-         <main className="dashboard">
-            <br />
-            <DarkMode />
-            <br />
-            <h1 className="dashboard">Welcome to Your Dashboard {name}</h1>
-            <br />
-            <User />
-            <br />
-            <Button
-               variant="contained"
-               color="primary"
-               type="submit"
-               onClick={onSubmit}
-            >
-               Edit Profile
-            </Button>
-            <br />
-            <br />
-         </main>
-      </>
-   );
+
+   let user_id = localStorage.getItem("user_id");
+   console.log(user_id);
+   {
+      const [updateUser] = useMutation(UPDATE_USER);
+      //let user_id = localStorage.getItem("user_id");
+      const onSubmit = ({
+         user_id = localStorage.getItem("user_id"),
+         first_name,
+         last_name,
+         username,
+         email,
+      }: any) => {
+         console.log(user_id);
+         console.log(users);
+         updateUser({
+            variables: { user_id, ...users },
+         })
+            .then(res => {
+               console.log(updateUser);
+               console.log(res.data);
+               localStorage.setItem("username", res.data.updateUser.username);
+               props.history.push("/home");
+               console.log("Successfully updated.");
+            })
+            .catch(err => {
+               alert(err.message);
+               console.log(err);
+            });
+      };
+      return (
+         <>
+            <main className="dashboard">
+               <br />
+               <DarkMode />
+               <br />
+               <h1 className="dashboard">Welcome to Your Dashboard {name}</h1>
+               <br />
+               <User />
+               <br />
+               <p>Update/Change </p>
+               <br />
+               <TextField
+                  label="username:"
+                  name="username"
+                  variant="filled"
+                  value={users.username}
+                  onChange={handleChange}
+               />
+               <TextField
+                  label="email:"
+                  name="email"
+                  variant="filled"
+                  value={users.email}
+                  onChange={handleChange}
+               />
+               <TextField
+                  label="first name"
+                  name="first_name"
+                  variant="filled"
+                  value={users.first_name}
+                  onChange={handleChange}
+               />
+               <TextField
+                  label="last name"
+                  name="last_name"
+                  variant="filled"
+                  value={users.last_name}
+                  onChange={handleChange}
+               />
+               <br />
+               <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  onClick={onSubmit}
+               >
+                  Edit Profile
+               </Button>
+               <br />
+               <br />
+            </main>
+         </>
+      );
+   }
 };
 
 export default Dashboard;
