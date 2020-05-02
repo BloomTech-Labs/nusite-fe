39f03b5e111d851272dad9a5077e85fc15ee1c81
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-05-02 17:35:15 -0500

    -Removed the loading component from index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,6 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import AppProviders from "./AppProviders";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import LoadingScreen from "./views/_shared/LoadingScreen";
-const AppProviders = lazy(() => import("./AppProviders"));
-//import { Loader } from "./views/_shared/Loader";
+import AppProviders from "./AppProviders";
 
-ReactDOM.render(
-   <Suspense fallback={<LoadingScreen />}>
-      <AppProviders />
-   </Suspense>,
-   document.getElementById("root")
-);
+ReactDOM.render(<AppProviders />, document.getElementById("root"));
