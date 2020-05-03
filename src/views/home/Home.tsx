cf4bb5f97ea5aa695d1aec485c3f0db95170e628
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-05-02 19:17:04 -0500

    -Refactored  Home componant to use global style.

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -6,32 +6,14 @@ import AboutSection from "./AboutSection";
 import PorjectOwnersSection from "./ProjectOnwersSection";
 import DevelopersSections from "./DevelopersSection";
 import Parallax from "./Parallax";
-const Hero = React.lazy(() => import("./Hero"));
-
-const useStyles = makeStyles(theme => ({
-   container: {
-      width: "100%",
-      padding: "0 20% 2.5% 20%",
-
-      [theme.breakpoints.down("md")]: {
-         padding: "0 15% 2.5% 15%",
-      },
-      [theme.breakpoints.down("sm")]: {
-         padding: "0 5% 2.5% 5%",
-      },
-      [theme.breakpoints.down("xs")]: {
-         padding: "0 2.5% 2.5% 2.5%",
-      },
-   },
-}));
+const Hero = lazy(() => import("./Hero"));
 
 const Home = (props: any) => {
-   const classes = useStyles();
    return (
       <>
          <Suspense fallback={<React.Fragment />}>
             <Hero className="hero" />
-            <Grid container className={classes.container} direction="column">
+            <Grid container className="main-container" direction="column">
                <AboutSection />
                <PorjectOwnersSection />
                <Parallax />
