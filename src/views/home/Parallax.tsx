9758b51fc65656663a94538b6ce11bf37cc7f002
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-05-07 17:12:16 -0500

    -Added parallax typing.

--- src/views/home/Parallax.tsx
+++ src/views/home/Parallax.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, makeStyles } from "@material-ui/core/";
+import Typed from "react-typed";
 
 const useStyles = makeStyles(theme => ({
    parallaxContainer: {
@@ -15,7 +16,7 @@ const useStyles = makeStyles(theme => ({
       width: "100%",
       height: "250px",
       backgroundImage:
-         "url('https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')",
+         "url('https://images.pexels.com/photos/316093/pexels-photo-316093.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
       backgroundAttachment: "fixed",
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
@@ -27,6 +28,19 @@ const useStyles = makeStyles(theme => ({
          height: "175px",
       },
    },
+
+   typingText: {
+      ...theme.overrides?.MuiTypography?.h1,
+      color: theme.palette.common.white,
+      fontWeight: 900,
+      display: "flex",
+      alignContent: "center",
+      justif: "center",
+      paddingLeft: "20px",
+      [theme.breakpoints.down("sm")]: {
+         fontSize: "1.1rem",
+      },
+   },
 }));
 
 const Parallax = (props: any) => {
@@ -34,7 +48,25 @@ const Parallax = (props: any) => {
 
    return (
       <Grid item className={classes.parallaxContainer}>
-         <div className={classes.parallaxBrackround}></div>
+         <Grid
+            container
+            alignContent="center"
+            className={classes.parallaxBrackround}
+         >
+            <Typed
+               className={classes.typingText}
+               strings={[
+                  "I need to build a new website",
+                  "I need to build a web application",
+                  "I need to revamp my old website",
+                  "I need a page for my buinsness",
+                  "I need a personal landing page",
+               ]}
+               typeSpeed={40}
+               backSpeed={50}
+               loop={true}
+            />
+         </Grid>
       </Grid>
    );
 };
