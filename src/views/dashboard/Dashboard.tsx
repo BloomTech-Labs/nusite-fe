d886bc9349031c0b17bfdede3edf982e4a804fd0
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-05-07 22:53:46 -0700

    fix: added background temp for login and registration until updated

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles(theme => ({
    textField: {
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
-      width: 200,
+      width: 250,
       alignItems: "left",
       textAlign: "center",
    },
