8ff53ebc6a2d8e5812e7c45c6763728c44445368
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-05-09 13:58:51 -0500

    misc: Attempting to use path to provide an absolute path to image resources.
    Co-authored-by: Bradley Pitts <BradleyJ.Pitts@gmail.com>
    Co-authored-by: Raymond Thurman <raymondthurman5@gmail.com>
    Co-authored-by: Joe Medrano <Joselmedrano@gmail.com>

--- src/views/_shared/OAuth/OAuthStyled.jsx
+++ src/views/_shared/OAuth/OAuthStyled.jsx
@@ -1,4 +1,36 @@
 import styled from "styled-components";
+// import L_Default from "../../../images/OAuth-Images/Linkedin-Default.png";
+// import L_Hover from "../../../images/OAuth-Images/Linkedin-Hover.png";
+// import L_Active from "../../../images/OAuth-Images/Linkedin-Active.png";
+import path from "path";
+
+// class Provider {
+//    constructor(...images) {
+//       this.default = images[0];
+//       this.hover = images[1];
+//       this.active = images[2];
+//    }
+//    getDefault() {
+//       return this.default;
+//    }
+//    getHover() {
+//       return this.hover;
+//    }
+//    getActive() {
+//       return this.active;
+//    }
+// }
+
+// const LinkedIn = new Provider(L_Default, L_Hover, L_Active);
+
+console.log("observe path module: ", path.dirname("/src/images/OAuth-Images/"));
+
+// const base = path.dirname("src/images/../images/OAuth-Images/");
+
+const background = (provider, state) => {
+   console.log("background source: ", provider, ": ", state);
+   return `url("../../../images/OAuth-Images/${provider}-${state}.png")`;
+};
 
 export const OAuthContainer = styled.div`
    width: 20rem;
@@ -12,16 +44,30 @@ export const OAuthContainer = styled.div`
 `;
 
 export const OAuthTag = styled.a`
-   background-image: ${props =>
-      `url("../../../images/OAuth-Images/${props.provider}-Default.png")`};
+   background-image: ${props => background(props.provider, "Default")};
    &:hover {
-      background-image: ${props =>
-         `url("../../../images/OAuth-Images/${props.provider}-Hover.png")`};
+      background-image: ${props => background(props.provider, "Hover")};
    }
    &:active {
-      background-image: ${props =>
-         `url("../../../images/OAuth-Images/${props.provider}-Active.png")`};
+      background-image: ${props => background(props.provider, "Active")};
    }
+
+   display: flex;
+   width: 12.5rem;
+   height: 2.5rem;
+   color: white;
+   margin: 0.5rem;
+   padding: 1rem;
+   padding-top: 1.25rem;
+   padding-right: 0.55rem;
+   border-radius: 0.2rem;
+   text-decoration: none;
+   font-size: 1rem;
+   justify-content: flex-end;
+   align-items: center;
+   background-repeat: no-repeat;
+   background-size: auto;
+   cursor: pointer;
 `;
 
 // export const OAuthTag = styled.a`
