7f819f8275865fdb0ce1ebe3c12e82744d016ea8
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-05-10 01:00:24 -0500

    fix: Fix still required in OAuthButton.jsx to refactor back to typeScript. Refactor required in OAuthStyled.jsx to refactor to TypeScript.

--- src/views/_shared/OAuthButton.jsx
+++ src/views/_shared/OAuthButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 // import { Providers } from "../../types/OAuthTypes";
 import { OAuthTag } from "./OAuth/OAuthStyled";
-// import "../_shared/OAuth/OAuth.css";
 
 export const OAuthButton = ({ provider }) => {
-   // export const OAuthButton = ({ provider }: Providers) => {
-   // const url: string = `${process.env.REACT_APP_BASE_URL}/api/auth/${provider}`;
    const url = `${process.env.REACT_APP_BASE_URL}/api/auth/${provider}`;
+
+   // export const OAuthButton = ({ provider }: Providers) => {
+   //    const url: string = `${process.env.REACT_APP_BASE_URL}/api/auth/${provider}`;
    return (
       <>
          <OAuthTag
@@ -17,14 +17,6 @@ export const OAuthButton = ({ provider }) => {
          >
             Log in with {provider}
          </OAuthTag>
-
-         {/* <a
-            href={url}
-            data-testid={`${provider} OAuth-button`}
-            className={`${provider} OAuth-button`}
-         >
-            Login with {provider}
-         </a> */}
       </>
    );
 };
