9e649cf0dc9133e729bbbf3f5d872eec90aa775c
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-05-12 12:14:08 -0500

    -Finished basic styling for register.

--- src/views/_shared/Input.tsx
+++ src/views/_shared/Input.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-
+import { TextField } from "@material-ui/core";
 export const Input = ({ register, errors, name, ...rest }: any) => {
    return (
       <>
-         <label>{rest.placeholder}</label>
-         <input
+         {/* <label>{rest.placeholder}</label> */}
+         <TextField
+            label={rest.label}
             name={name}
-            ref={register({
+            inputRef={register({
                required: true,
                minLength: rest.minLength || null,
             })}
+            variant="outlined"
             errors={errors}
             {...rest}
          />
--- src/views/registration/Registration.tsx
+++ src/views/registration/Registration.tsx
@@ -3,8 +3,7 @@ import { Form } from "../_shared/Form";
 import { Input } from "../_shared/Input";
 import { SIGNUP } from "../../graphql-requests/mutations";
 import { useMutation } from "@apollo/react-hooks";
-import Button from "@material-ui/core/Button";
-import "./Registration.css";
+import { Button, Grid, makeStyles, Typography } from "@material-ui/core/";
 import Loader from "../_shared/Loader";
 import { RegistrationFormData } from "../../types/FormTypes";
 import {
@@ -14,14 +13,54 @@ import {
 } from "../../context/user/actions";
 import UserContext from "../../context/user/context";
 import GoogleOAuth from "../_shared/GoogleOAuth";
-//
+
+const useStyles = makeStyles(theme => ({
+   container: {
+      height: "1000px",
+      backgroundImage: `url(${require("../../images/register.jpeg")})`,
+      backgroundSize: "cover",
+
+      [theme.breakpoints.down("md")]: {
+         backgroundPosition: "bottom",
+      },
+   },
+   registerContainer: {
+      backgroundColor: "white",
+      width: "500px",
+      border: "1px",
+      color: theme.palette.primary.light,
+      borderStyle: "solid",
+      borderRadius: "25px",
+      [theme.breakpoints.down("md")]: {
+         maxWidth: "100%",
+      },
+   },
+   formContainer: {
+      padding: "50px",
+      flexDirection: "column",
+      display: "flex",
+      alignItems: "center",
+      justify: "center",
+      alignContent: "center",
+   },
+   textField: {
+      marginBottom: "3em",
+      [theme.breakpoints.down("md")]: {
+         width: "100%",
+      },
+   },
+   oAuthButton: {
+      marginBottom: "3em",
+   },
+}));
 
 export const Registration: React.FC<RegistrationFormData> = (
    props: RegistrationFormData | any
 ) => {
    const { userData, userDispatch } = useContext(UserContext);
-
+   const classes = useStyles();
    const [signup] = useMutation(SIGNUP);
+
    const onSubmit = ({
       first_name,
       last_name,
@@ -55,38 +94,88 @@ export const Registration: React.FC<RegistrationFormData> = (
 
    return (
       <>
-         <div className="box">
-            <Form
-               className="register-form"
-               data-testid="register-form"
-               onSubmit={onSubmit}
+         <Grid
+            container
+            direction="column"
+            alignItems="center"
+            className={`main-container ${classes.container}`}
+         >
+            <Grid
+               container
+               direction="column"
+               className={classes.registerContainer}
+               alignItems="center"
+               margin-bottom={8}
             >
-               <Input type="text" name="first_name" placeholder="First Name" />
-               <Input type="text" name="last_name" placeholder="Last Name" />
-               <Input
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  autoComplete="username"
-               />
-               <Input
-                  name="password"
-                  placeholder="Password"
-                  autoComplete="current-password"
-                  type="password"
-                  minLength={9}
-               />
-               <Input name="email" placeholder="Email" type="email" />
-               {userData.isAuthorizing ? (
-                  <Loader />
-               ) : (
-                  <Button variant="contained" color="secondary" type="submit">
+               <Grid item>
+                  <Typography variant="h1" align="center">
                      Register
-                  </Button>
-               )}
-            </Form>
-            <GoogleOAuth />
-         </div>
+                  </Typography>
+               </Grid>
+
+               <Form
+                  className={classes.formContainer}
+                  data-testid="register-form"
+                  onSubmit={onSubmit}
+               >
+                  <Input
+                     type="text"
+                     name="first_name"
+                     label="First Name"
+                     className={classes.textField}
+                  />
+                  <Input
+                     type="text"
+                     name="last_name"
+                     label="Last Name"
+                     className={classes.textField}
+                  />
+                  <Input
+                     type="text"
+                     name="username"
+                     label="Username"
+                     fullWidth
+                     autoComplete="username"
+                     className={classes.textField}
+                  />
+                  <Input
+                     name="password"
+                     label="Password"
+                     autoComplete="current-password"
+                     type="password"
+                     fullWidth
+                     minLength={9}
+                     className={classes.textField}
+                  />
+                  <Input
+                     name="email"
+                     label="Email"
+                     type="email"
+                     fullWidth
+                     className={classes.textField}
+                  />
+                  <Grid container justify="center" alignItems="center">
+                     {userData.isAuthorizing ? (
+                        <Loader />
+                     ) : (
+                        <Button
+                           variant="contained"
+                           color="primary"
+                           type="submit"
+                        >
+                           <Typography variant="body1" color="secondary">
+                              Submit
+                           </Typography>
+                        </Button>
+                     )}
+                  </Grid>
+               </Form>
+
+               <Grid className={classes.oAuthButton}>
+                  <GoogleOAuth />
+               </Grid>
+            </Grid>
+         </Grid>
       </>
    );
 };
