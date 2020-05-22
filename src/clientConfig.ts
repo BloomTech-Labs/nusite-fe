32bccca33e7926a9f6cd6e9b3f406de8643443ad
BPitts8019 <bradleyj.pitts@gmail.com>
2020-05-21 17:24:28 -0700

    :bug: Correct error in BASE_URL

--- src/clientConfig.ts
+++ src/clientConfig.ts
@@ -1,2 +1,3 @@
-export const BASE_URL: string = `${process.env.REACT_APP_BASE_URL} || localhost:4000`;
+export const BASE_URL: string = `${process.env.REACT_APP_BASE_URL ||
+   "localhost:4000"}`;
 export const APOLLO_CLIENT_URL: string = `${BASE_URL}/graphql`;
