aa301ff150ddb87ccd9955d890d91c90ae2212df
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-05-21 21:15:39 -0700

    chore: make routes uniform

--- src/views/dashboard/Profile.tsx
+++ src/views/dashboard/Profile.tsx
@@ -207,12 +207,12 @@ const Profile: React.FC = (props: any) => {
                      <Link to="/chat">
                         <li className="sidenav__list-item">Chat</li>
                      </Link>
-                     {/* <Link to="/marketplace">
+                     <Link to="/marketplace">
                         <li className="sidenav__list-item">Marketplace</li>
-                     </Link> */}
-                     <Link to="/">
-                        <li className="sidenav__list-item">Homepage</li>
                      </Link>
+                     {/* <Link to="/">
+                        <li className="sidenav__list-item">Homepage</li>
+                     </Link> */}
                   </ul>
                </aside>
             </div>
